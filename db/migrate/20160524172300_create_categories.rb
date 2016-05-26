276659a43d1f5b9d8583604791e7b10c6ac32e21
maysam <kherad@gmail.com>
2016-05-26 15:29:22 +0430

    fixing pg migration problem

--- db/migrate/20160524172300_create_categories.rb
+++ db/migrate/20160524172300_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-      t.references :parent, index: true, foreign_key: true
+      t.references :parent, index: true
       t.string :name
 
       t.timestamps null: false
