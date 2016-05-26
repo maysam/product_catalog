bb31d278230876c2f634db0bae035ad68d76a272
maysam <kherad@gmail.com>
2016-05-26 13:27:54 +0430

    a few tests to support the category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,7 +7,7 @@ class Category < ActiveRecord::Base
 
   validates :name, uniqueness: true
 
-  scope :top_level, -> { where(parent: nil) }
+  scope :top_level, -> { where parent: nil }
   scope :sub_level, -> { top_level.map(&:children).flatten }
   scope :end_level, -> { sub_level.map(&:children).flatten }
   scope :parent_to_be, -> { top_level + sub_level }
@@ -29,9 +29,5 @@ class Category < ActiveRecord::Base
 
   def to_s
     name
-  end
-
-  def is_parentable
-    parent_id.nil? or parent.parent_id.nil?
   end
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert Product.count == 2
+  test "the counts" do
+    assert_equal 3, Category.with_product.count
+
+    assert_equal 3, categories(:one).all_product_ids.flatten.uniq.count
+    assert_equal 2, categories(:two).all_product_ids.flatten.uniq.count
+    assert_equal 1, categories(:three).all_product_ids.flatten.uniq.count
+    assert_equal 0, categories(:empty).all_product_ids.flatten.uniq.count
+
+    assert_equal 3, Category.parent_to_be.count
+    assert_equal 2, Category.top_level.count
+    assert_equal 1, Category.sub_level.count
+    assert_equal 1, Category.end_level.count
   end
 end
